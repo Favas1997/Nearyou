814350a3f446ca39002ab7fd53e4170df94778aa
muhammad favas <favasm123@gail.com>
2021-07-05 09:02:29 +0530

    changed secret key

--- consulting/settings.py
+++ consulting/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config("SECRET_KEY")
 
-SECRET_KEY= config("SECRET_KEY")
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ["139.59.31.45",]
 
